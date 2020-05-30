79983063dd930ab837a70c86c316ddc27333c04b
EmmanuelAyTuz <tuzemmanuel@gmail.com>
2020-05-30 14:43:59 -0500

    Test

--- test/example.py
+++ test/example.py
@@ -2,8 +2,12 @@ from pyswip import Prolog
 prolog = Prolog()
 prolog.assertz("father(michael,john)")
 prolog.assertz("father(michael,gina)")
-list(prolog.query("father(michael,X)")) == [{'X': 'john'}, {'X': 'gina'}]
-for soln in prolog.query("father(X,Y)"):
-    print(soln["X"], "is the father of", soln["Y"])
+#list(prolog.query("father(michael,X)")) == [{'X': 'john'}, {'X': 'gina'}]
+#for soln in prolog.query("father(X,Y)"):
+#    print(soln["X"], "is the father of", soln["Y"])
+
+for children in prolog.query("father(michael, Y)"):
+    print("Hijos de Michael: ", children["Y"])
+
 # michael is the father of john
 # michael is the father of gina
