84929e2f07fae26a1b4c325978962cd550d0221e
EmmanuelAyTuz <tuzemmanuel@gmail.com>
2020-05-30 16:05:15 -0500

    Subjects

---
+++ src/subjects.pl
@@ -0,0 +1,6 @@
+%Materias de la carrera
+
+%Clave,Nombre,Semestre[1-9]Carrera['Sistemas'=>1],creditos
+
+subject("ACA0907", "TALLER DE ÉTICA", 1, 1, 3).
+subject("ACA0909", "TALLER DE INV. I", 4, 1, 4).
