d4a9a94d2cbd146aa6cb6e6953bece4b32547546
EduardoUnSuaste <emanuelun18@gmail.com>
2020-05-30 18:12:08 -0500

    06:11
    Materias cargadas

--- src/subjects.pl
+++ src/subjects.pl
@@ -10,8 +10,26 @@ subject('ACC0906', 'FUNDAMENTOS DE INV', 1, 1, 4).
 subject('ACD0908', 'DES. SUSTENTABLE', 3, 1, 4).
 subject('ACA0901', 'CALCULO DIFERENCIAL', 1, 1, 5).
 subject('ACF0902', 'CALCULO INTEGRAL', 2, 1, 4).
-subject('ACF0903', 'CÁLCULO DIFERENCIAL', 7, 1, 4).
-subject('ACA0901', 'CÁLCULO DIFERENCIAL', 7, 1, 5).
+subject('ACF0903', 'ALGEBRA LINEAL', 2, 1, 4).
+subject('ACF0904', 'CALCULO VECTORIAL', 3, 1, 3).
+subject('ACF0905', 'ECUACIONES DIFEREN', 4, 1, 3).
+subject('AEB1055', 'PROGRAMACION WEB', 6, 1, 5).
+subject('AEC1008', 'CONTABILIDAD FINANC', 2, 1, 3).
+subject('AEC1034', 'FUND. DE TELECOM', 3, 1, 4).
+subject('AEC1058', 'QUIMICA', 1, 1, 5).
+subject('AEC1061', 'SIST. OPERATIVOS I', 4, 1, 4).
+subject('AEF1031', 'FUND. B. DATOS', 4, 1, 3).
+subject('AEF1041', 'MATEMATICAS DISCRET', 1, 1, 5).
+subject('AEF1052', 'PROB. Y ESTADISTICA', 2, 1, 3).
+subject('SCD1023', 'SIST. PROGRAMABLES', 7, 1, 4).
+subject('SCG1009', 'G. DE P. DE SOFTWARE', 7, 1, 5).
+subject('SCD1015', 'LENG. Y AUTÓMATAS I', 7, 1, 4).
+subject('TEA1702', 'DES. APLIC MOVILES I', 7, 1, 4).
+subject('TEC1703', 'COMPUTO EN LA NUBE', 7, 1, 5).
+subject('SCD1016', 'LENG. Y AUTOMAT. II', 6, 1, 3).
+subject('SCD1022', 'SIMULACION', 5, 1, 3).
+subject('SCD1027', 'TOP. AV. DE PROG.', 4, 1, 3).
+subject('SSO2010', 'SERV. SOCIAL', 7, 1, 5).
 
 
 
