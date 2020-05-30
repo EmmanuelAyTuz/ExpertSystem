4b2c2e3e051d69894d91618c1af4ca7f84b0de42
EmmanuelAyTuz <tuzemmanuel@gmail.com>
2020-05-30 17:23:09 -0500

    Subjects

--- src/subjects.pl
+++ src/subjects.pl
@@ -1,6 +1,6 @@
+%:- style_check(-singleton).
 %Materias de la carrera
 
 %Clave,Nombre,Semestre[1-9]Carrera['Sistemas'=>1],creditos
-
-subject("ACA0907", "TALLER DE ÉTICA", 1, 1, 3).
-subject("ACA0909", "TALLER DE INV. I", 4, 1, 4).
+subject('ACA0907', 'TALLER DE ETICA', 1, 1, 3).
+subject('ACA0909', 'TALLER DE INV. I', 4, 1, 4).
