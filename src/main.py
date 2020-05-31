beca57377c7f1ed94262a26dc9d6780eef4feb78
EduardoUnSuaste <emanuelun18@gmail.com>
2020-05-30 20:39:54 -0500

    Cambio SWITCH
    MENU

--- src/main.py
+++ src/main.py
@@ -7,25 +7,57 @@ print(welcome)
 
 prolog.consult("src/subjects.pl")
 
-print("#### MATERIAS ####")
-for sb in prolog.query("subject(A, B, C, D, E)."):
-    print("Clave: ", sb["A"], " Nombre: ", sb["B"], " Semestre: ",
-          sb["C"], " Carrera: ", sb["D"], " Creditos: ", sb["E"])
+def TOTAL():
+    print("#### MATERIAS ####")
+    for sb in prolog.query("subject(A, B, C, D, E)."):
+        print("Clave: ", sb["A"], " Nombre: ", sb["B"], " Semestre: ",
+        sb["C"], " Carrera: ", sb["D"], " Creditos: ", sb["E"])
 
-print("#### FILTRAR POR SEMESTRE ####")
-sm = input("Numero de semestre: ")
-for sb in prolog.query("subject(A, B, " + sm + ", D, E)."):
-    print("Clave: ", sb["A"], " Nombre: ", sb["B"],
-          " Carrera: ", sb["D"], " Creditos: ", sb["E"])
+def SEMESTRE():
+    print("#### FILTRAR POR SEMESTRE ####")
+    sm = input("Numero de semestre: ")
+    for sb in prolog.query("subject(A, B, " + sm + ", D, E)."):
+        print("Clave: ", sb["A"], " Nombre: ", sb["B"],
+        " Carrera: ", sb["D"], " Creditos: ", sb["E"])
 
-print("#### FILTRAR POR CARRERA ####")
-crr = input("Numero de carrera: ")
-for sb in prolog.query("subject(A, B, C, " + crr + ", E)."):
-    print("Clave: ", sb["A"], " Nombre: ", sb["B"], " Semestre: ",
-          sb["C"], " Creditos: ", sb["E"])
+def CARRERA():
+    print("#### FILTRAR POR CARRERA ####")
+    crr = input("Numero de carrera: ")
+    for sb in prolog.query("subject(A, B, C, " + crr + ", E)."):
+        print("Clave: ", sb["A"], " Nombre: ", sb["B"], " Semestre: ",
+        sb["C"], " Creditos: ", sb["E"])
 
-print("#### FILTRAR POR CREDITOS ####")
-cr = input("Cantidad de creditos: ")
-for sb in prolog.query("subject(A, B, C, D, " + cr + ")."):
-    print("Clave: ", sb["A"], " Nombre: ", sb["B"], " Semestre: ",
-          sb["C"], " Carrera: ", sb["D"])
+def CREDITOS():
+    print("#### FILTRAR POR CREDITOS ####")
+    cr = input("Cantidad de creditos: ")
+    for sb in prolog.query("subject(A, B, C, D, " + cr + ")."):
+        print("Clave: ", sb["A"], " Nombre: ", sb["B"], " Semestre: ",
+        sb["C"], " Carrera: ", sb["D"])
+
+def default():
+    return "Opcion Invalida"
+
+def switch(case):
+    sw = {
+    1: TOTAL(),
+    2: SEMESTRE(),
+    3: CARRERA(),
+    4: CREDITOS(),
+    }
+    return sw.get(case, default())
+
+
+def menu():
+    print("----------- MENU INICIAL -----------")
+    print("1. TOTAL DE MATERIAS")
+    print("2. FILTRAR SEMESTRE")
+    print("3. FILTRAR CARRERA")
+    print("4. FILTRAR CREDITOS")
+    print("-----------------------------------")
+
+
+menu()
+case = int(input("Seleccione una opcion: "))
+print(switch(case))
+
+
